b8a02ff38492f2a757172abaa6ea3c12cfd541b7
Kennethaustin <kenneth.austin93@gmail.com>
2019-08-09 16:09:09 -0400

    finished

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FormikForm from './Components/NewUserForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <FormikForm/>
+    </div>
+  );
+}
+
+export default App;
